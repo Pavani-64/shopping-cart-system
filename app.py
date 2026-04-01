0e348c2d95685476025d24b6488f9a85397d02cb
Pavani <tarra.pavani2004@gmail.com>
2026-04-01 13:02:51 +0530

    Added requirements.txt

--- app.py
+++ app.py
@@ -112,4 +112,7 @@ def logout():
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
